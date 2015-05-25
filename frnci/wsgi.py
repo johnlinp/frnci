f978b94d6a09c26f2c232b1ecacbda259847a50d
johnlinp <johnlinp@gmail.com>
2015-05-25 23:02:35 +0800

    Serve static files

--- frnci/wsgi.py
+++ frnci/wsgi.py
@@ -11,4 +11,5 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "frnci.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(get_wsgi_application())
