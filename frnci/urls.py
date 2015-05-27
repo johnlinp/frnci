1f3b278ae0f970641c404a101b7e6337f9beb800
johnlinp <johnlinp@gmail.com>
2015-05-28 01:06:55 +0800

    Add find local page

--- frnci/urls.py
+++ frnci/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'landing.views.home', name='home'),
+    url(r'^locals$', 'landing.views.locals', name='locals'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
