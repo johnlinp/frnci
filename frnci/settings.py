cc531d05309eec015af1296dcc74e6242bb08094
johnlinp <johnlinp@gmail.com>
2015-06-05 23:07:16 +0800

    i18n for index #4

--- frnci/settings.py
+++ frnci/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'landing',
+    'landing',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -47,6 +47,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
 )
 
 ROOT_URLCONF = 'frnci.urls'
